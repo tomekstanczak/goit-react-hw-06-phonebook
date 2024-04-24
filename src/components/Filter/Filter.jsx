8134bcdf751fe3735497051864c1cd7fde9502c0
tomekstanczak <tomek.stanczak87@gmail.com>
2024-04-24 07:32:27 +0200

    css

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,15 @@
+import css from './Filter.module.css';
+
 export function Filter({ handleChange }) {
   return (
     <>
       <h2>Find contact</h2>
-      <input type="text" name="filter" onChange={handleChange} />
+      <input
+        type="text"
+        name="filter"
+        onChange={handleChange}
+        className={css.filter}
+      />
     </>
   );
 }
